5bf55579e066b52fe42efb0443ca871412e389fd
Rhudá Oliveira Campos <rhudazaum@gmail.com>
2021-02-13 15:22:02 -0300

    Configurando conexoes

--- DesafioFULL.Web/Startup.cs
+++ DesafioFULL.Web/Startup.cs
@@ -67,7 +67,8 @@ namespace DesafioFULL.Web
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
                 }
             });
         }
