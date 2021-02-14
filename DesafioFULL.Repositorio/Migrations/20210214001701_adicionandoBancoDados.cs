5bd9f2716bc0e10380ae790b5ac94b1141a98601
Rhudá Oliveira Campos <rhudazaum@gmail.com>
2021-02-13 21:26:37 -0300

    Adicionado primeiro migration

---
+++ DesafioFULL.Repositorio/Migrations/20210214001701_adicionandoBancoDados.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesafioFULL.Repositorio.Migrations
+{
+    public partial class adicionandoBancoDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    SobreNome = table.Column<string>(maxLength: 100, nullable: true),
+                    CPF = table.Column<string>(maxLength: 11, nullable: false),
+                    Fone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    Senha = table.Column<string>(maxLength: 400, nullable: false),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    SobreNome = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Titulos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClienteId = table.Column<long>(nullable: false),
+                    QtdeParcelas = table.Column<int>(nullable: false),
+                    PerJuros = table.Column<decimal>(nullable: false),
+                    PerMulta = table.Column<decimal>(nullable: false),
+                    VlrOriginal = table.Column<decimal>(nullable: false),
+                    VlrCorrigido = table.Column<decimal>(nullable: false),
+                    DiasEmAtraso = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Titulos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Titulos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TituloParcelas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TituloId = table.Column<long>(nullable: false),
+                    NumParcela = table.Column<int>(nullable: false),
+                    Vencimento = table.Column<DateTime>(nullable: false),
+                    VlrOriginal = table.Column<decimal>(nullable: false),
+                    VlrCorrigido = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TituloParcelas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TituloParcelas_Titulos_TituloId",
+                        column: x => x.TituloId,
+                        principalTable: "Titulos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_CPF",
+                table: "Clientes",
+                column: "CPF",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TituloParcelas_TituloId",
+                table: "TituloParcelas",
+                column: "TituloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Titulos_ClienteId",
+                table: "Titulos",
+                column: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TituloParcelas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Titulos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
